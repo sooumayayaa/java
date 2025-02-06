aa76cd32a7e9645d3a92453054b9e72f5c4263bb
Soumya Aggarwal <soumyaaggarwal3494@gmail.com>
2025-02-06 15:29:40 +0530

    lab3 exp1

--- Sum.java
+++ Sum.java
@@ -1,33 +1,26 @@
 import java.util.ArrayList;
 
 public class Sum {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 950;
+        int sum = 0;
 
-        /**
-         * @param args
-         */
-        public static void main(String[] args)
-{
-    int x = 10;
-    int y = 950;
-    int i;
-    int sum = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
 
-    ArrayList<Integer> arr = new ArrayList<>();
-    for (i = x; i <= y;  i++)
-    {
-       if (i % 6 == 0 && i % 9 == 0)
-       {
-           arr.add(i); // add i to our array
-       }
-  
-    // sum of all the elemnets of the new array
-    for(int num) 
-    {
-        num+=sum;
-     }
-}
-     
-System.out.println("Sum of all numbers divisible by 6 and 9 from "+ x +" to " + y + " is: " + sum);
+        // Find numbers divisible by 6 and 9
+        for (int i = x; i <= y; i++) {
+            if (i % 6 == 0 && i % 9 == 0) {
+                arr.add(i); // Add i to our array
+            }
+        }
 
-}
+        // Sum of all the elements in the array
+        for (int num : arr) {
+            sum += num;
+        }
+
+        // Print the result
+        System.out.println("Sum of all numbers divisible by 6 and 9 from " + x + " to " + y + " is: " + sum);
+    }
 }
